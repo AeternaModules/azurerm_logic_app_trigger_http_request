709ac38049bf1142652b54af3d00be44b4b125fa
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:39 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "logic_app_trigger_http_requests" {
-  description = "All logic_app_trigger_http_request resources"
-  value       = azurerm_logic_app_trigger_http_request.logic_app_trigger_http_requests
-}
 output "logic_app_trigger_http_requests_callback_url" {
-  description = "List of callback_url values across all logic_app_trigger_http_requests"
-  value       = [for k, v in azurerm_logic_app_trigger_http_request.logic_app_trigger_http_requests : v.callback_url]
+  description = "Map of callback_url values across all logic_app_trigger_http_requests, keyed the same as var.logic_app_trigger_http_requests"
+  value       = { for k, v in azurerm_logic_app_trigger_http_request.logic_app_trigger_http_requests : k => v.callback_url }
 }
 output "logic_app_trigger_http_requests_logic_app_id" {
-  description = "List of logic_app_id values across all logic_app_trigger_http_requests"
-  value       = [for k, v in azurerm_logic_app_trigger_http_request.logic_app_trigger_http_requests : v.logic_app_id]
+  description = "Map of logic_app_id values across all logic_app_trigger_http_requests, keyed the same as var.logic_app_trigger_http_requests"
+  value       = { for k, v in azurerm_logic_app_trigger_http_request.logic_app_trigger_http_requests : k => v.logic_app_id }
 }
 output "logic_app_trigger_http_requests_method" {
-  description = "List of method values across all logic_app_trigger_http_requests"
-  value       = [for k, v in azurerm_logic_app_trigger_http_request.logic_app_trigger_http_requests : v.method]
+  description = "Map of method values across all logic_app_trigger_http_requests, keyed the same as var.logic_app_trigger_http_requests"
+  value       = { for k, v in azurerm_logic_app_trigger_http_request.logic_app_trigger_http_requests : k => v.method }
 }
 output "logic_app_trigger_http_requests_name" {
-  description = "List of name values across all logic_app_trigger_http_requests"
-  value       = [for k, v in azurerm_logic_app_trigger_http_request.logic_app_trigger_http_requests : v.name]
+  description = "Map of name values across all logic_app_trigger_http_requests, keyed the same as var.logic_app_trigger_http_requests"
+  value       = { for k, v in azurerm_logic_app_trigger_http_request.logic_app_trigger_http_requests : k => v.name }
 }
 output "logic_app_trigger_http_requests_relative_path" {
-  description = "List of relative_path values across all logic_app_trigger_http_requests"
-  value       = [for k, v in azurerm_logic_app_trigger_http_request.logic_app_trigger_http_requests : v.relative_path]
+  description = "Map of relative_path values across all logic_app_trigger_http_requests, keyed the same as var.logic_app_trigger_http_requests"
+  value       = { for k, v in azurerm_logic_app_trigger_http_request.logic_app_trigger_http_requests : k => v.relative_path }
 }
 output "logic_app_trigger_http_requests_schema" {
-  description = "List of schema values across all logic_app_trigger_http_requests"
-  value       = [for k, v in azurerm_logic_app_trigger_http_request.logic_app_trigger_http_requests : v.schema]
+  description = "Map of schema values across all logic_app_trigger_http_requests, keyed the same as var.logic_app_trigger_http_requests"
+  value       = { for k, v in azurerm_logic_app_trigger_http_request.logic_app_trigger_http_requests : k => v.schema }
 }
 
